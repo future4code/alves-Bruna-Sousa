775d3578f05d863954cc746655e8c38a0cf307ff
Bruna Vitória Oliveira de Sousa <brunavitoria@msn.com>
2022-06-13 23:48:35 -0300

    rend-conticional-form

---
+++ modulo2/rend-condicional-form/rend-condicional-form/src/components/agradecimento.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container1 = styled.div`
+ Display: flex;
+ margin: 5px;
+`
+
+
+   
+   render () {
+    return (
+      <Container1>
+        <h1>Muito Obrigada suas informações foram coletadas!!</h1>
+      </Container1>
+    );
+   }
+ }
