8d87e01d6b57c5a79faa579c3bbbd89018f70dae
Bruna Vitória Oliveira de Sousa <brunavitoria@msn.com>
2022-09-13 03:14:47 -0300

    atualizar

---
+++ modulo5/introducao-autenticacao/src/table.sql
@@ -0,0 +1,6 @@
+-- Active: 1662330059941@@35.226.146.116@3306@alves-bruna-sousa
+CREATE TABLE Usuariointro (
+		id VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
